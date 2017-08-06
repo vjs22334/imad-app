d73b1d0c1a995d16abefba7c90bf9608cb21e53d
vjs22334 <30682201+vjs22334@users.noreply.github.com>
2017-08-06 13:59:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article 1', function (req, res) {
+  res.sendFile(path.join(__dirname,  'art1.html'));
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
