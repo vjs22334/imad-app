b63b4a4694535b8aff0e0cfe64fe801fbcaa05dc
vjs22334 <30682201+vjs22334@users.noreply.github.com>
2017-08-07 20:07:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ button.onclick=function(){
         if(request.readystate===XMLHttpRequest.DONE){
             if(request.state===200){
                 var counter = request.responseText;    
-                span.innerHTML=counter.toString();
+                span.innerHTML=counter;
             }
         }
     
