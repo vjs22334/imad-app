709020921bb0edfd2d6824f6efa93746adb75e33
vjs22334 <30682201+vjs22334@users.noreply.github.com>
2017-08-07 20:35:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ button.onclick=function(){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(request.readyState===XMLHttpRequest.DONE){
-            if(request.state===200)
+            if(request.status===200)
             {
                 var span = document.getElementById('countervalue');
                 span.innerHTML = request.responseText.toString();
