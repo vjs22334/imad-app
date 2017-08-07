cbaa736864738c06cbc1d1df9b1caecfa8865ae1
vjs22334 <30682201+vjs22334@users.noreply.github.com>
2017-08-07 20:11:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,12 @@
 
 var button=document.getElementById('counter');
-
+var counter=0;
 button.onclick=function(){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(request.readystate===XMLHttpRequest.DONE){
             if(request.state===200){
-                var counter = request.responseText;
+                counter = counter +1;
                 var span=document.getElementById('countervalue');
                 span.innerHTML = counter.toString();
             }
