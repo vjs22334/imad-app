2419de6697df17da86d4e13e6cc036275f67dda9
vjs22334 <30682201+vjs22334@users.noreply.github.com>
2017-08-07 20:32:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
-var counter=0;
+
 var button=document.getElementById('counter');
 button.onclick=function(){
-    /*var request = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(request.readyState===XMLHttpRequest.DONE){
             if(request.state===200){
@@ -10,12 +10,12 @@ button.onclick=function(){
             }
         }
     
-    }
+    };
     request.open('GET',"http://jeyasuryav.imad.hasura-app.io/counter");
-    request.send(null);*/
-    counter=counter+1;
+    request.send(null);
+    /*counter=counter+1;
     var span = document.getElementById('countervalue');
-    span.innerHTML=counter.toString();
+    span.innerHTML=counter.toString();*/
     
     
 };
