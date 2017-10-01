42088afe7c19cdcc4a9c7060a9ee85cde3402a13
vjs22334 <30682201+vjs22334@users.noreply.github.com>
2017-10-01 20:16:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,6 +99,7 @@ res.send(JSON.stringify(carea));
 app.get('/articles/:articleName', function (req, res) {
     var articleName=req.params.articleName;
     var data;
+    console.log(data);
     pool.query("SELECT * FROM articles WHERE title = '"+articleName+"'",function(err,result){
         if(err){
             res.status(500).send(err.toString());
