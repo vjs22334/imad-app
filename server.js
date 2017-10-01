03e531324f95262ebd85026c1e157c31c851f278
vjs22334 <30682201+vjs22334@users.noreply.github.com>
2017-10-01 19:23:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var config = {
 };
 var pool = new Pool({config});
 app.get('/test-db',function(req,res){
-    pool.query('SELECT * FROM "articles"',function(err,result)
+    pool.query('SELECT * FROM articles',function(err,result)
     {
         if(err){
             res.status(500).send(err.toString());
